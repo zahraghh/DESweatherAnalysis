03bc36abf5c54809304a00ae10e6be53888a5eda
zahraghh <42415558+zahraghh@users.noreply.github.com>
2022-06-17 09:19:59 -0600

    Add files via upload

--- Test_Case/Pre_Analysis.py
+++ Test_Case/Pre_Analysis.py
@@ -6,7 +6,7 @@ import csv
 from pathlib import Path
 import subprocess
 import DES_weather_analysis
-from DES_weather_analysis import download_windsolar_data, weather_file_PDFs, scenario_generation_MCMC,scenario_analysis_MCMC,create_epws,eppy_connect_Eplus
+from DES_weather_analysis import download_windsolar_data, weather_file_PDFs, scenario_generation_MCMC_interdependent,scenario_analysis_MCMC_interdependent,create_epws,eppy_connect_Eplus,clustring_kmediod_operation
 
 
 path_test =  os.path.join(sys.path[0])
@@ -18,11 +18,11 @@ start_year = int(editable_data['starting_year'])
 end_year = int(editable_data['ending_year'])
 num_scenarios = int(editable_data['num_scenarios'])
 if __name__ == "__main__":
-    download_windsolar_data.download_meta_data(city_DES)
-    weather_file_PDFs.weather_PDS_data(path_test)
-    scenario_generation_MCMC.scenario_generation_results(path_test)
-    scenario_analysis_MCMC.scenario_analysis(path_test)
-    create_epws.create_epw_files(path_test)
-    eppy_connect_Eplus.run_Eplus(path_test)
+    #download_windsolar_data.download_meta_data(city_DES)
+    #weather_file_PDFs.weather_PDS_data(path_test)
+    #scenario_generation_MCMC_interdependent.scenario_generation_results(path_test)
+    #scenario_analysis_MCMC_interdependent.scenario_analysis(path_test)
+    #create_epws.create_epw_files(path_test)
+    #eppy_connect_Eplus.run_Eplus(path_test)
     clustring_kmediod_operation.kmedoid_clusters(path_test,'total')
     clustring_kmediod_operation.kmedoid_clusters(path_test,'seperate')
